36fed224717bd70e8594cba75c5bb63f676efb66
Fatos Ismali <fatos_i@live.com>
2012-02-08 21:36:59 +0000

    added rails proj

---
+++ kclandroid/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kclandroid::Application.config.secret_token = '12aeb97a928be147968d7e5cec9438fa19d801ca128df0091988c18cfc58a0a092ab1aaccab037d0ae8a2a1279874207e43508ea6cfd9ae17025f915222a25d4'
